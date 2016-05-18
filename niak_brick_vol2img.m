7dc2ce5e3cc29eb4f9522a52cff60f060aa9d93d
Pierre Bellec <pierre.bellec@gmail.com>
2016-05-17 21:57:25 -0400

    Added the possibility to generate a mosaic with no decoration (i.e. no axis, colorbar, etc).

--- niak_brick_vol2img.m
+++ niak_brick_vol2img.m
@@ -12,13 +12,16 @@ function [in,out,opt] = niak_brick_vol2img(in,out,opt)
 % OPT.COLORMAP (string, default 'gray') The type of colormap. Anything supported by 
 %   the instruction `colormap` will work. 
 % OPT.TITLE (string, default '') a title for the figure. 
+% OPT.SIZE_SLICES (vector 1x2) the size of each slice (X,Y,Z) in the mosaic.
 % OPT.LIMITS (vector 1x2) the limits for the colormap. By defaut it is using [min,max].
 %    If a string is specified, the function will implement an adaptative strategy. 
+% FLAG_DECORATION (boolean, default true) if the flag is true, produce a regular figure
+%    with axis, title and colorbar. Otherwise just output the plain mosaic.
 % OPT.FLAG_TEST (boolean, default false) if the flag is true, the brick does nothing but 
 %    update IN, OUT and OPT.
 %
 % Copyright (c) Pierre Bellec
-% Centre de recherche de l'Institut universitaire de gériatrie de Montréal, 2016.
+% Centre de recherche de l'Institut universitaire de griatrie de Montral, 2016.
 % Maintainer : pierre.bellec@criugm.qc.ca
 % See licensing information in the code.
 % Keywords : visualization, montage, 3D brain volumes
@@ -51,8 +54,8 @@ if isempty(in.target)
 end
 
 opt = psom_struct_defaults ( opt , ...
-    { 'colorbar' , 'coord' , 'limits' , 'colormap' , 'title' , 'flag_test' }, ...
-    { true         , NaN     , []        , 'gray'         , ''       , false         });
+    { 'colorbar' , 'coord' , 'limits' , 'colormap' , 'size_slices' , 'title' , 'flag_decoration' , 'flag_test' }, ...
+    { true       , NaN     , []       , 'gray'     , []            , ''      , true              , false         });
 
 if opt.flag_test 
     return
@@ -66,21 +69,23 @@ ext_f = ext_f(2:end);
 hdr.target = niak_read_vol(in.target);
 [hdr.source,vol] = niak_read_vol(in.source);
 
-%% Generate the image
-for cc = 1:size(opt.coord,1)
-    [img_tmp,slices] = niak_vol2img(hdr,vol,opt.coord(cc,:));
-    if cc == 1
-        img = img_tmp;
-        size_slices = size(slices{1});
-        size_slices = [size_slices ; size(slices{2})];
-        size_slices = [size_slices ; size(slices{3})];
-    else
-        img = [img ; img_tmp];
+
+%% Build image
+for tt = 1:size(vol,4)
+    for cc = 1:size(opt.coord,1)
+        [img_tmp,slices] = niak_vol2img(hdr,vol(:,:,:,tt),opt.coord(cc,:));
+        if (cc == 1)&&(tt==1)
+            img = img_tmp;
+            size_slices = size(slices{1});
+            size_slices = [size_slices ; size(slices{2})];
+            size_slices = [size_slices ; size(slices{3})];
+        else
+            img = [img ; img_tmp];
+        end
     end
 end
 
-%% The image
-hf = figure;
+%% image limits
 if ischar(opt.limits)
     mask = niak_mask_brain(vol);
     mvol = median(vol(mask));
@@ -94,6 +99,25 @@ if isempty(opt.limits)
 else
     climits = opt.limits;
 end
+
+%% No decoration: generate a bare mosaic
+if ~opt.flag_decoration
+    img(img>climits(2)) = climits(2);
+    img(img<climits(1)) = climits(1);
+    cm = colormap(opt.colormap);
+    bins = linspace(climits(1),climits(2),size(cm,1));
+    [tmp,idx] = histc(img,bins);
+    idx(idx==0) = 1;
+    rgb = zeros([size(img),3]);
+    rgb(:,:,1) = reshape(cm(idx(:),1),size(img));
+    rgb(:,:,2) = reshape(cm(idx(:),2),size(img));
+    rgb(:,:,3) = reshape(cm(idx(:),3),size(img));
+    imwrite(rgb,out);
+    return
+end
+
+%% Build a figure
+hf = figure;
 imagesc(img,climits);
 
 %% Colorbar/map
@@ -105,7 +129,7 @@ end
 
 %% Title
 if ~isempty(opt.title)
-    title(strrep(opt.title,'_','\_');
+    title(strrep(opt.title,'_','\_'));
 end
 
 %% Set X axis 
@@ -133,9 +157,6 @@ set(ha,'yticklabel',label_view);
 %set(FN,'FontName','/usr/share/fonts/truetype/dejavu/DejaVuSerifCondensed.ttf');
 FS = findall(ha,'-property','FontSize');
 set(FS,'FontSize',8);
-
-%% Get rid of unecessary white space
-niak_tightfig(hf);
 
 %% Save figure
 print(out,['-d' ext_f]);
